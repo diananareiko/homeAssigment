839ebe4da8e49b21beee7cf74f4abe193f8eab45
d.nareyko <d.nareyko@itransition.com>
2025-02-23 11:49:21 +0100

    Initial Commit

---
+++ HomeAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeAssignment
+//
+//  Created by Nareyko, Diana on 23.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
